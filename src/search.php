2e098ead988a6f3e920172202b9d4a146b5cf99d
sarabashirieh <sara.bashiri@gmail.com>
2014-03-31 14:12:09 +0200

    error fixing?

--- src/search.php
+++ src/search.php
@@ -2,12 +2,12 @@
 session_start();
 //echo "hello";
 //echo $_SESSION['userID'];
-$userID = $_SESSION['userID'];
-if(!isset($_SESSION['userID'])){
+// $userID = $_SESSION['userID'];
+// if(!isset($_SESSION['userID'])){
 
-$userID = 1;
-//echo $userID;
-}
+// $userID = 1;
+// //echo $userID;
+// }
 
 ?>
 <!DOCTYPE html>
@@ -141,36 +141,36 @@ $userID = 1;
 //             print_r($value1->{'rating'}->value);
 // echo "string";
 //              print_r($res->value);
-             if($res->value == 5){
-               echo '<fieldset class="rating1"><input type="radio" id="star5" name="rating" value="5" /><label for="star5" title="Pretty good">5 stars</label>
-              <input type="radio" id="star4" name="rating" value="4" /><label for="star4" title="Pretty good">4 stars</label>
-              <input type="radio" id="star3" name="rating" value="3" /><label for="star3" title="Meh">3 stars</label>
-              <input type="radio" id="star2" name="rating" value="2" /><label for="star2" title="Kinda bad">2 stars</label>
-              <input type="radio" id="star1" name="rating" value="1" /><label for="star1" title="Bad">1 star</label></fieldset>';
+             // if($res->value == 5){
+             //   echo '<fieldset class="rating1"><input type="radio" id="star5" name="rating" value="5" /><label for="star5" title="Pretty good">5 stars</label>
+             //  <input type="radio" id="star4" name="rating" value="4" /><label for="star4" title="Pretty good">4 stars</label>
+             //  <input type="radio" id="star3" name="rating" value="3" /><label for="star3" title="Meh">3 stars</label>
+             //  <input type="radio" id="star2" name="rating" value="2" /><label for="star2" title="Kinda bad">2 stars</label>
+             //  <input type="radio" id="star1" name="rating" value="1" /><label for="star1" title="Bad">1 star</label></fieldset>';
              
-             }
-             elseif($res->value == 4){
+             // }
+             // elseif($res->value == 4){
 
-              echo '<fieldset class="rating1"><input type="radio" id="star4" name="rating" value="4" /><label for="star4" title="Pretty good">4 stars</label>
-              <input type="radio" id="star3" name="rating" value="3" /><label for="star3" title="Meh">3 stars</label>
-              <input type="radio" id="star2" name="rating" value="2" /><label for="star2" title="Kinda bad">2 stars</label>
-              <input type="radio" id="star1" name="rating" value="1" /><label for="star1" title="Bad">1 star</label></fieldset>';
-             }
-             elseif($res->value == 3){
-              echo '<fieldset class="rating1"><input type="radio" id="star3" name="rating" value="3" /><label for="star3" title="Meh">3 stars</label>
-              <input type="radio" id="star2" name="rating" value="2" /><label for="star2" title="Kinda bad">2 stars</label>
-              <input type="radio" id="star1" name="rating" value="1" /><label for="star1" title="Bad">1 star</label></fieldset>';
-             }
-              elseif($res->value == 2){
-              echo '<fieldset class="rating1"><input type="radio" id="star2" name="rating" value="2" /><label for="star2" title="Kinda bad">2 stars</label>
-              <input type="radio" id="star1" name="rating" value="1" /><label for="star1" title="Bad">1 star</label></fieldset>';
-             }
-              elseif($res->value == 1){
-              echo '<fieldset class="rating1"><input type="radio" id="star1" name="rating" value="1" /><label for="star1" title="Bad">1 star</label></fieldset>';
-             }
-             else{
-              echo'No ranting available';
-             }
+             //  echo '<fieldset class="rating1"><input type="radio" id="star4" name="rating" value="4" /><label for="star4" title="Pretty good">4 stars</label>
+             //  <input type="radio" id="star3" name="rating" value="3" /><label for="star3" title="Meh">3 stars</label>
+             //  <input type="radio" id="star2" name="rating" value="2" /><label for="star2" title="Kinda bad">2 stars</label>
+             //  <input type="radio" id="star1" name="rating" value="1" /><label for="star1" title="Bad">1 star</label></fieldset>';
+             // }
+             // elseif($res->value == 3){
+             //  echo '<fieldset class="rating1"><input type="radio" id="star3" name="rating" value="3" /><label for="star3" title="Meh">3 stars</label>
+             //  <input type="radio" id="star2" name="rating" value="2" /><label for="star2" title="Kinda bad">2 stars</label>
+             //  <input type="radio" id="star1" name="rating" value="1" /><label for="star1" title="Bad">1 star</label></fieldset>';
+             // }
+             //  elseif($res->value == 2){
+             //  echo '<fieldset class="rating1"><input type="radio" id="star2" name="rating" value="2" /><label for="star2" title="Kinda bad">2 stars</label>
+             //  <input type="radio" id="star1" name="rating" value="1" /><label for="star1" title="Bad">1 star</label></fieldset>';
+             // }
+             //  elseif($res->value == 1){
+             //  echo '<fieldset class="rating1"><input type="radio" id="star1" name="rating" value="1" /><label for="star1" title="Bad">1 star</label></fieldset>';
+             // }
+             // else{
+             //  echo'No ranting available';
+             // }
              echo'
               </td>
             </tr>';
