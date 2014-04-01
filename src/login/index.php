48569c606df6d1d4bc5da2c70cb0bcd0cb5f9b89
sarabashirieh <sara.bashiri@gmail.com>
2014-04-01 11:29:58 +0200

    login tweak

--- src/login/index.php
+++ src/login/index.php
@@ -107,7 +107,7 @@ echo $userID;
 <form role="form" method="POST" action="/index.php">
         Welcome to crowdchef, please login to continue
 <div class="form-group">
-           <label for="exampleInputEmail1">Username</label><input type="email" class="form-control" id="exampleInputEmail1" name="username"/>
+           <label for="exampleInputEmail1">Username</label><input type="text" class="form-control" id="exampleInputEmail1" name="username"/>
         </div>
         <div class="form-group">
            <label for="exampleInputPassword1">Password</label><input type="password" class="form-control" id="exampleInputPassword1" name="password" />
